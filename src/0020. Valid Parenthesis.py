bd2d00c2007286194e59d44e3282c2370f55fe11
Solomon0519 <ng.eswen1@gmail.com>
2025-03-14 15:41:25 +0800

    Update 0020 Valid Parenthesis

--- src/0020. Valid Parenthesis.py
+++ src/0020. Valid Parenthesis.py
@@ -28,4 +28,38 @@ class Solution:
             return False
         else:
             return True
+        
+from collections import deque
+
+# Alternate Solution
+
+# class Solution:
+#     def isValid(self, s: str) -> bool:
+
+#         if len(s) % 2 == 1:
+#             return False
+
+#         # Initialise a deques to iterate through the string and store the
+#         # open  parenthses
+#         open_parentheses = deque([])
+#         deque_length = 0
+
+#         valid_open = ['(', '{', '[']
+#         valid_close = ['()', '{}', '[]']
+
+#         for character in s:
+#             if character in valid_open:
+#                 open_parentheses.append(character)
+#                 deque_length += 1
+#             else:
+#                 if deque_length == 0:
+#                     return False
+#                 else:
+#                     if open_parentheses.pop() + character in valid_close:
+#                         deque_length -= 1
+#                         continue
+#                     else:
+#                         return False
+
+#         return deque_length == 0
 
